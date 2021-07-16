2e8770cdc4e82e2fb0a880356fa871dd29e4b0e0
lawzem <lawrence.zemour@exagroup.biz>
2021-07-16 15:50:13 +0200

    won't use api anymore...

---
+++ src/projects/resolvers/deleteProject.ts
@@ -0,0 +1,6 @@
+import { Context } from '../../server';
+import { Mutation, MutationDeleteProjectArgs } from '../../types';
+
+export default async (parent: Mutation, args: MutationDeleteProjectArgs, context: Context) => {
+  return null;
+};
